95dff876250fe865446a96ad37b86d6581949a3b
fanc1 <fanxu.bot@gmail.com>
2024-08-27 14:21:56 +0800

    update: add login and register pages todo: add auth logic

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Imprima } from "next/font/google";
+import { cn } from "@/lib/utils";
+const state = true;
+
+export default function Home() {
+  return <div>This is a protected route.</div>;
+}
