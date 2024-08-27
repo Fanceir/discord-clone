9be38b1b6d59b5eaa3c30a0b2a0f1723c3d7937a
fanc1 <fanxu.bot@gmail.com>
2024-08-27 15:40:37 +0800

    更新sign-in和sign-up页面 todo 完成侧边栏

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Imprima } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 const state = true;
 
 export default function Home() {
-  return <div>This is a protected route.</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </div>
+  );
 }
