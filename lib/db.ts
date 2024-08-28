a165c0edad7432ef14fe24e73c03da12cb46fcd2
fanc1 <fanxu.bot@gmail.com>
2024-08-28 18:25:30 +0800

    更新网站的标志

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,11 @@
-import { Prisma } from "@prisma/client";
-import { PrismaClient } from "@prisma/client/extension";
-import exp from "constants";
+import { PrismaClient } from "@prisma/client";
+
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
   globalThis.prisma = db;
 }
